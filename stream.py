335357efc4baf9128458852ae33f5c7b8fa42e1b
ayarivery <117346028+ayarivery@users.noreply.github.com>
2022-11-03 11:32:28 +0200

    Update stream.py
    change index in list

--- stream.py
+++ stream.py
@@ -12,4 +12,4 @@ import pandas as pd
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.1))
