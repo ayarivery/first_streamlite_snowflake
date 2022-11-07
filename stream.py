b53b6225aac6776287d0177071188d0c5b620094
ayarivery <117346028+ayarivery@users.noreply.github.com>
2022-11-07 09:51:20 +0200

    Update stream.py
    normalize and insert the json response into data frame

--- stream.py
+++ stream.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+# normalaize the jason response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# insert the response into data frame
+streamlit.dataframe(fruityvice_normalized)
